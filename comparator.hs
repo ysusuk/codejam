023fb57dff89aa921aa0a4b45aad279f51a43011
Yuriy Susuk <ysusuk@autoscout24.com>
2016-04-21 17:36:05 +0200

    comparator extended

--- comparator.hs
+++ comparator.hs
@@ -3,5 +3,12 @@ import System.Environment (getArgs)
 main = do
   args <- getArgs
   case args of
-    [file1, file2] -> putStrLn "done"
+    [file1, file2] -> do
+      in1 <- readFile file1
+      in2 <- readFile file2
+      let
+        result x y = (if x /= y then "\n/=" else "") ++ "\n" ++ x ++ "\n" ++ y
+        out = zipWith result (lines in1) (lines in2)
+      putStrLn $ unlines out
+
     _ -> putStrLn "error: exactly two arguments needed"
